6ce183df5f3fde3197d667cb059bcc5f0a5b514d
sakuya10969 <sakuya.f20010618@gmail.com>
2025-05-19 18:29:23 +0900

    fix: constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,10 @@
 export const GRADES = [
-  { value: '1', label: '学部1年生' },
-  { value: '2', label: '学部2年生' },
-  { value: '3', label: '学部3年生' },
-  { value: '4', label: '学部4年生' },
-  { value: '5', label: '修士1年生' },
-  { value: '6', label: '修士2年生' }
+  { value: '1', label: '学士1年' },
+  { value: '2', label: '学士2年' },
+  { value: '3', label: '学士3年' },
+  { value: '4', label: '学士4年' },
+  { value: '5', label: '修士1年' },
+  { value: '6', label: '修士2年' }
 ] as const;
 
 export type Grade = typeof GRADES[number]['value'];
